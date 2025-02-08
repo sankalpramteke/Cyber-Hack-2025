6303dda627d305565c954fbfb94bb9f7cd2ca932
sankalpramteke <116703076+sankalpramteke@users.noreply.github.com>
2025-02-08 07:24:46 +0530

    changes

---
+++ src/pages/CsamCases.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const CsamCases: React.FC = () => {
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold mb-6">CSAM Cases</h1>
+      <div className="bg-white dark:bg-gray-800 rounded-lg shadow p-6">
+        <div className="space-y-4">
+          <p className="text-gray-600 dark:text-gray-300">
+            This page is under development. CSAM (Child Sexual Abuse Material) cases will be displayed here.
+          </p>
+          {/* Add your CSAM cases content here */}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CsamCases;
